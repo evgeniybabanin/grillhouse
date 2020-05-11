29d389b028d79336beaac51a8afe567bb388a036
Evgeniy Babanin <yevgeniybabanin@gmail.com>
2020-05-11 14:08:30 +0300

    add a project Grillhouse

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider_wrapper').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+  });
